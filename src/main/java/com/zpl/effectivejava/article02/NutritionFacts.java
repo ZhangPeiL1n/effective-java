e7c4210ccb7f37146c3d84b3219d8a0dbf9f9c05
张沛霖 <zhangpeilinn@163.com>
2020-09-24 17:16:20 +0800

    创建项目

---
+++ src/main/java/com/zpl/effectivejava/article02/NutritionFacts.java
@@ -0,0 +1,69 @@
+package com.zpl.effectivejava.article02;
+
+/**
+ * @author 张沛霖
+ * @date 2020/9/24
+ */
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+    }
+
+    /**
+     * 使用静态内部类来初始化各个属性
+     */
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            this.calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            this.fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            this.sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            this.carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+}
